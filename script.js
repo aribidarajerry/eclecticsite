39afaa3f695240a41861a3c44cfefbc082d7f88f
* <aribidarajerry@gmail.com>
2024-09-19 21:22:53 +0100

    new update

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 $(document).ready(function() {
-    $(".fa-bars").on("click", function() {
-        $("nav").width("100%");
-        $("#logo").css("visibility", "hidden");
-        $(this).hide();
-        $(".fa-close").show();
-    })
-    $(".fa-close").on("click", function() {
-        $("nav").width("0");
-        $("#logo").css("visibility", "visible");
-        $(this).hide();
-        $(".fa-bars").show();
-    })
-    $(".portfolio").hover(function() {
-        $(".portfolio p").slideDown();
-    }, function() {
-        $(".portfolio p").slideUp();
-    })
-    $(".data").hover(function() {
-        $(".data p").slideDown();
-    }, function() {
-        $(".data p").slideUp();
-    })
-    $(".calculator").hover(function() {
-        $(".calculator p").slideDown();
-    }, function() {
-        $(".calculator p").slideUp();
-    })
-    $(".princess").hover(function() {
-        $(".princess p").slideDown();
-    }, function() {
-        $(".princess p").slideUp();
-    })
-    $(".resume").hover(function() {
-        $(".resume p").slideDown();
-    }, function() {
-        $(".resume p").slideUp();
-    })
+	$(".fa-bars").on("click", function() {
+		$(this).hide();
+		$(".fa-close").show();
+		$("nav").css({
+			"width": "50%", "display": "flex"
+		});
+	})
+	$(".fa-close").on("click", function() {
+		$(this).hide();
+		$(".fa-bars").show();
+		$("nav").css("width", "0%");
+	})
+	$(".portfolio").hover(function() {
+		$(".portfolio p").slideDown();
+	}, function() {
+		$(".portfolio p").slideUp();
+	})
+	$(".data").hover(function() {
+		$(".data p").slideDown();
+	}, function() {
+		$(".data p").slideUp();
+	})
+	$(".calculator").hover(function() {
+		$(".calculator p").slideDown();
+	}, function() {
+		$(".calculator p").slideUp();
+	})
+	$(".princess").hover(function() {
+		$(".princess p").slideDown();
+	}, function() {
+		$(".princess p").slideUp();
+	})
+	$(".resume").hover(function() {
+		$(".resume p").slideDown();
+	}, function() {
+		$(".resume p").slideUp();
+	})
 })
